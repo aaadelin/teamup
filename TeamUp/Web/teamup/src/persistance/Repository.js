20d3f0cdd65a5f5109f9a2e41e9fe24cd3271fa2
Adelin Mirea <adelinmirea@gmail.com>
2019-08-15 16:21:15 +0300

    Added SSL
    Added SSL on backend to run on https on port 8443

--- TeamUp/Web/teamup/src/persistance/Repository.js
+++ TeamUp/Web/teamup/src/persistance/Repository.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseDomain = 'http://192.168.0.150:8081'
+const baseDomain = 'https://192.168.0.150:8443'
 export const baseURL = `${baseDomain}/api`
 
 export default axios.create({
